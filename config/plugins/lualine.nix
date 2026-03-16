3257edeecabd41cc88fded820834aef4f88183b8
Robert Schaffar-Taurok <robert@projektbuero.at>
2026-03-16 20:09:38 +0100

    Add macro recording indicator to lualine via Noice mode status

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -3,6 +3,21 @@
     enable = true;
     settings = {
       options.theme = "auto";
+      sections = {
+        lualine_x = [
+          {
+            __unkeyed-1.__raw = ''
+              require("noice").api.status.mode.get
+            '';
+            cond.__raw = ''
+              require("noice").api.status.mode.has
+            '';
+            color = {
+              fg = "#ff9e64";
+            };
+          }
+        ];
+      };
       tabline = {
         lualine_a = [
           {
