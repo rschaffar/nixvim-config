912d1516909f71fcfd63b5392592176c7a897537
Robert Schaffar-Taurok <robert@projektbuero.at>
2026-04-29 14:42:07 +0200

    temporarily disable nvim-ufo with TODO

--- config/plugins/ufo.nix
+++ config/plugins/ufo.nix
@@ -1,16 +1,17 @@
 {
-  plugins.nvim-ufo = {
-    enable = true;
-
-    settings = {
-      provider_selector = ''
-        function(bufnr, filetype, buftype)
-          if filetype == "python" then
-            return { "indent" }
-          end
-          return { "lsp", "indent" }
-        end
-      '';
-    };
-  };
+  # TODO(rs): Re-enable nvim-ufo once the async.nvim / promise-async conflict is fixed.
+  # plugins.nvim-ufo = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     provider_selector = ''
+  #       function(bufnr, filetype, buftype)
+  #         if filetype == "python" then
+  #           return { "indent" }
+  #         end
+  #         return { "lsp", "indent" }
+  #       end
+  #     '';
+  #   };
+  # };
 }
